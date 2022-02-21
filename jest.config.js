d90eeae6ea0c55e3516445385f9673e0203a36de
LeCuay <suso.becerra98@gmail.com>
2022-02-21 14:10:30 +0000

    fix(test): add `Constants` to mapper
    Forgot to add `@Constants` to mapper so Jest can read it.
    
    Changes to be committed:
    	modified:   jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@Components/(.*)$': '<rootDir>/src/components/$1',
+    '^@Constants$': '<rootDir>/src/const/globalConst',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
