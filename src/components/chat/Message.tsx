cd2bc4749aa738a6a4ed7138e342d39347f05ecb
LeCuay <suso.becerra98@gmail.com>
2022-03-02 15:51:59 +0000

    feat(component): `Message` component
    Added `Message` component but tests are needed.
    
    Changes to be committed:
    	new file:   src/components/chat/Message.tsx

---
+++ src/components/chat/Message.tsx
@@ -0,0 +1,88 @@
+import dayjs from 'dayjs';
+
+import React, { FC, useContext } from 'react';
+import PropTypes, { InferProps } from 'prop-types';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import { AuthContext } from '@Contexts';
+
+const MessageProps = {
+  message: PropTypes.shape({
+    author: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      username: PropTypes.string.isRequired,
+    }).isRequired,
+    message: PropTypes.string.isRequired,
+    entry_created_at: PropTypes.string.isRequired,
+  }).isRequired,
+  colWidthXS: PropTypes.number,
+  colWidthMD: PropTypes.number,
+};
+
+const MessageDefaults = {
+  colWidthXS: 10,
+  colWidthMD: 8,
+};
+
+type MessageTypes = InferProps<typeof MessageProps>;
+const Message: FC<MessageTypes> = ({ message, colWidthXS, colWidthMD }) => {
+  const user = useContext(AuthContext);
+
+  const renderMessage = () => (
+    <>
+      <p className="mb-0">
+        <small className="text-white font-weight-bold">
+          <u>{message.author.username}</u>
+        </small>
+        <br />
+        {message.message}
+      </p>
+      <p className="text-right mb-0">
+        <small className="text-muted">
+          Sent at{' '}
+          {dayjs(message.entry_created_at).format('DD/MM/YYYY HH:mm:ss')}
+        </small>
+      </p>
+    </>
+  );
+
+  if (message.author.id === user?.id) {
+    return (
+      <Row
+        className="justify-content-end m-0 mb-2"
+        style={{ minHeight: '50px' }}
+      >
+        <Col
+          xs={colWidthXS || 0}
+          md={colWidthMD || 0}
+          className="bg-secondary border"
+          style={{ borderRadius: '10px' }}
+        >
+          {renderMessage()}
+        </Col>
+      </Row>
+    );
+  }
+
+  return (
+    <Row
+      className="justify-content-start m-0 mb-2"
+      style={{ minHeight: '50px' }}
+    >
+      <Col
+        xs={colWidthXS || 0}
+        md={colWidthMD || 0}
+        className="bg-info border"
+        style={{ borderRadius: '10px' }}
+      >
+        {renderMessage()}
+      </Col>
+    </Row>
+  );
+};
+
+Message.propTypes = MessageProps;
+Message.defaultProps = MessageDefaults;
+
+export default Message;
