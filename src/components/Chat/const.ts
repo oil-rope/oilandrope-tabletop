c4bec1f7d704aa65b1c3f512e798c6d9bb4effce
LeCuay <suso.becerra98@gmail.com>
2022-04-08 08:51:11 +0100

    fix(env): new environment variable
    Added new environment variable.
    Also some information on `Chat/const.ts`.
    
    Changes to be committed:
    	modified:   .env.example
    	modified:   .github/workflows/node.yml
    	modified:   .github/workflows/node_dev.yml
    	modified:   src/components/Chat/const.ts

--- src/components/Chat/const.ts
+++ src/components/Chat/const.ts
@@ -1,3 +1,4 @@
+// See https://javascript.plainenglish.io/typescript-tip-convert-an-array-of-strings-into-a-string-literal-union-type-ee46c2c6e337
 export const MESSAGE_COLORS = [
   'success',
   'info',
