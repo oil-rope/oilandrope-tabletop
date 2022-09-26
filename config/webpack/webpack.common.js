1ef4fc2b48061ae9c7d4142b3c95634dab0389ab
LeCuay <suso.becerra98@gmail.com>
2022-09-26 10:53:52 +0100

    refactor(OAR-94): reestrcuture project
    Just working around all needs.
    
    Changes to be committed:
    	modified:   .github/workflows/node.yml
    	modified:   config/webpack/webpack.common.js
    	deleted:    config/webpack/webpack.dev.js
    	deleted:    config/webpack/webpack.mix.js
    	deleted:    config/webpack/webpack.prod.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/index.html
    	deleted:    public/vendor/422.ccc9a9f4.bundle.js
    	deleted:    public/vendor/872.7b85b8da.bundle.js
    	deleted:    public/vendor/index.html
    	deleted:    public/vendor/oilandrope_tabletop.bundle.js
    	deleted:    public/vendor/oilandrope_tabletop.bundle.js.LICENSE.txt
    	modified:   tsconfig.json
    	new file:   tsconfig.webpack.json

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,66 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { EnvironmentPlugin, DefinePlugin } = require('webpack');
-require('dotenv').config();
 
 const BASE_PATH = path.resolve(__dirname, '../../');
 const SOURCE_PATH = path.join(BASE_PATH, 'src/');
-const PUBLIC_PATH = path.join(BASE_PATH, 'public/');
-
-const HtmlWebpackPluginConf = new HtmlWebpackPlugin({
-  template: path.join(PUBLIC_PATH, 'index.html'),
-  filename: path.join(PUBLIC_PATH, 'vendor/index.html'),
-});
-let EnvironmentPluginConf;
-
-if (process.env.NODE_ENV == 'production') {
-  EnvironmentPluginConf = new DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production'),
-    'process.env.API_URL': JSON.stringify('https://oilandrope-project.com'),
-    'process.env.WS_URL': JSON.stringify('wss://live.oilandrope-project.com'),
-  });
-} else {
-  EnvironmentPluginConf = new EnvironmentPlugin([
-    'API_URL',
-    'WS_URL',
-    'NODE_ENV',
-  ]);
-}
 
 module.exports = {
   entry: {
     oilandrope_tabletop: path.join(SOURCE_PATH, 'index.tsx'),
   },
-  output: {
-    path: path.join(PUBLIC_PATH, 'vendor/'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].[contenthash:8].bundle.js',
-    clean: true,
-    publicPath: '/',
-  },
+  devtool: 'inline-source-map',
+  mode: 'development',
   module: {
     rules: [
       {
-        test: /\.ts(x)?$/,
-        use: 'ts-loader',
-        exclude: [/node_modules/, /tests/],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'img/[name][ext][query]',
-        },
+        test: /\.tsx?$/,
+        // NOTE: Exclude, source-map and others are tackled by `webpack.tsconfig.json`
+        use: 'ts-loader?configFile=tsconfig.webpack.json',
       },
     ],
   },
-  performance: {
-    assetFilter: (assetFilename) => !/\.(map|png)$/.test(assetFilename),
-    hints: 'error',
-  },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
       '@': SOURCE_PATH,
       '@Components': path.join(SOURCE_PATH, 'components/'),
@@ -70,5 +30,8 @@ module.exports = {
       '@Interfaces': path.join(SOURCE_PATH, 'interfaces'),
     },
   },
-  plugins: [HtmlWebpackPluginConf, EnvironmentPluginConf],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
--- config/webpack/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { merge } = require('webpack-merge');
-
-const common = require('./webpack.common');
-
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: false,
-});
