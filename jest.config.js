4339b5689d3c84d784f7a06832b35415317c8400
LeCuay <suso.becerra98@gmail.com>
2022-10-22 16:33:41 +0100

    feat(support): `.browserslistrc`
    Added browser support through `browserslist`.
    Also changed some config using `create-react-app` as reference.
    
    Changes to be committed:
    	new file:   .browserslistrc
    	modified:   jest.config.js
    	deleted:    jestSetup.js
    	new file:   setupTests.ts

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,17 @@ const { compilerOptions } = require('./tsconfig.json');
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const jestConfig = {
+  roots: ['<rootDir>/src'],
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.tsx',
+  ],
   coverageDirectory: 'coverage',
   moduleDirectories: [...defaults.moduleDirectories, 'src'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
-  setupFilesAfterEnv: ['<rootDir>/jestSetup.js'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).[jt]s(x)?'],
 };
